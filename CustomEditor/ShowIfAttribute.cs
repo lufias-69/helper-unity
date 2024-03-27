6829d87258bfd0e259f11324c9a145914bd744a1
L U F i A S <Lufias4november@gmail.com>
2024-03-27 19:46:01 +0600

    Update ShowIfAttribute.cs

--- CustomEditor/ShowIfAttribute.cs
+++ CustomEditor/ShowIfAttribute.cs
@@ -7,10 +7,30 @@ using UnityEngine;
 [System.AttributeUsage(System.AttributeTargets.Field)]
 public class ShowIfAttribute : PropertyAttribute
 {
-    public string boolFieldName;
-    public ShowIfAttribute(string boolFieldName)
+    public enum ConditionType
     {
-        this.boolFieldName = boolFieldName;
+        Boolean,
+        Integer,
+        Float,
+        Enum        
+    }
+
+    public string fieldName;
+    public ConditionType conditionType;
+    public object conditionValue;
+
+    public ShowIfAttribute(string boolVariableName)
+    {
+        fieldName = boolVariableName;
+        conditionType = ConditionType.Boolean;
+        conditionValue = true;
+    }
+
+    public ShowIfAttribute(string fieldName, ConditionType conditionType, object conditionValue)
+    {
+        this.fieldName = fieldName;
+        this.conditionType = conditionType;
+        this.conditionValue = conditionValue;
     }
 }
 
@@ -21,42 +41,51 @@ public class ShowIfDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         ShowIfAttribute showIf = attribute as ShowIfAttribute;
-        SerializedProperty boolProperty = property.serializedObject.FindProperty
-            (showIf.boolFieldName);
-        if (boolProperty == null)
+        SerializedProperty conditionProperty = property.serializedObject.FindProperty(showIf.fieldName);
+
+        if (conditionProperty == null)
         {
             EditorGUI.PropertyField(position, property, label, true);
+            return;
         }
-        else
+
+        bool showProperty = CheckCondition(conditionProperty, showIf.conditionType, showIf.conditionValue);
+
+        if (showProperty)
         {
-            bool value = boolProperty.boolValue;
-            if (value)
-            {
-                EditorGUI.PropertyField(position, property, label, true);
-            }
+            EditorGUI.PropertyField(position, property, label, true);
         }
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         ShowIfAttribute showIf = attribute as ShowIfAttribute;
-        SerializedProperty boolProperty = property.serializedObject.FindProperty
-            (showIf.boolFieldName);
-        if (boolProperty == null)
+        SerializedProperty conditionProperty = property.serializedObject.FindProperty(showIf.fieldName);
+
+        if (conditionProperty == null || CheckCondition(conditionProperty, showIf.conditionType, showIf.conditionValue))
         {
             return EditorGUI.GetPropertyHeight(property, label);
         }
         else
         {
-            bool value = boolProperty.boolValue;
-            if (value)
-            {
-                return EditorGUI.GetPropertyHeight(property, label);
-            }
-            else
-            {
-                return 0f;
-            }
+            return 0f;
+        }
+    }
+
+    bool CheckCondition(SerializedProperty conditionProperty, ShowIfAttribute.ConditionType conditionType, object conditionValue)
+    {
+        switch (conditionType)
+        {
+            case ShowIfAttribute.ConditionType.Boolean:
+                return conditionProperty.boolValue == (bool)conditionValue;
+            case ShowIfAttribute.ConditionType.Integer:
+                return conditionProperty.intValue == (int)conditionValue;
+            case ShowIfAttribute.ConditionType.Float:
+                return conditionProperty.floatValue == (float)conditionValue;
+            case ShowIfAttribute.ConditionType.Enum:
+                return conditionProperty.enumValueIndex == (int)conditionValue;            
+            default:
+                return false;
         }
     }
 }
