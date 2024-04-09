f4e33ed95437075d8635f3b23a0119efb3567ec2
L U F i A S <saifulanik@icloud.com>
2024-04-09 20:56:05 +0600

    missing script remover

---
+++ Editor/MissingScriptRemover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Helper
+{
+    public static class MissingScriptsRemover
+    {
+        [MenuItem("Helper/Remove Missing Scripts")]
+        public static void RemoveMissing()
+        {
+            GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+            int removedCount = 0;
+
+            foreach (GameObject go in allObjects)
+            {
+                // Remove missing scripts
+                int count = GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);                
+
+                if(count > 0)
+                {
+                    Debug.Log("Found missing scripts on " + go.name);
+                    removedCount += count;
+                }
+            }
+
+            Debug.Log("Removed missing scripts from " + removedCount + " GameObjects in the scene.");
+        }
+    }
+}
+
+
